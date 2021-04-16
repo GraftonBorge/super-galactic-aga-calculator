3d45a4bed9994c487840c470a02b0d8bf1d97919
GraftonBorge <graftonmborge@gmail.com>
2021-04-16 16:44:41 -0700

    add last commit for solo project

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -7,9 +7,9 @@ describe ('galacticAgeCalculator', () => {
     myAges = new galacticAgeCalculator(24,{"planetName":"calculatedAge"})
   });
 
-  Test('should create a galacticAgeCalculator object', () => {
-    expect(myAges.inputtedAge).toEqual(24)
-    expect(myAges.planets).toEqual({"planetName":"calculatedAge"})
+  test('should create a galacticAgeCalculator object', () => {
+    expect(myAges.inputtedAge).toEqual(24);
+    expect(myAges.planets).toEqual({"planetName":"calculatedAge"});
   })
 
   //   test ('it should take inputed age and should return age', () => {
---
+++ src/js/super-galactic-age-calculator.js
@@ -0,0 +1,7 @@
+export default class galacticAgeCalculator {
+
+  constructor(inputtedAge, planets) {
+    this.age = inputtedAge;
+    this.planets = planets;
+  }
+}
