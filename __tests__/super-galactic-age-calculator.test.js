f1ea54a530803c8d2ac4f9cc036b3c9268a397e6
GraftonBorge <graftonmborge@gmail.com>
2021-04-16 09:18:35 -0700

    add new test to begin with

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -1,6 +1,18 @@
-describe ('galacticAgeCalculator', () => {
+import galacticAgeCalculator from './../src/js/super-galactic-age-calculator';
 
-    test ('it should take inputed age and should return age', () => {
-      expect (inputedAge).toEqual(24);
-    });
+describe ('galacticAgeCalculator', () => {
+  let myAges;
+
+  beforeEach(() => {
+    myAges = new galacticAgeCalculator(24,{"planetName":"calculatedAge"})
+  });
+
+  Test('should create a galacticAgeCalculator object', () => {
+    expect(myAges.inputtedAge).toEqual(24)
+    expect(myAges.planets).toEqual({"planetName":"calculatedAge"})
+  })
+
+  //   test ('it should take inputed age and should return age', () => {
+  //     expect (myAge.inputtedAge).toEqual(24);
+  //   });
   })
