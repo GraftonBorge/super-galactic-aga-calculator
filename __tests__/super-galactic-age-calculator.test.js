7d2f43ad0017c8db60cb4a644c3769841d2f913a
GraftonBorge <graftonmborge@gmail.com>
2021-04-16 08:18:32 -0700

    test: it should take inputed age and should return age

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -1,5 +1,6 @@
-describe("TEMPLATE TEST", () => {
-  test("sanity check", () => {
-    expect(true).toEqual(true)
+describe ('galacticAgeCalculator', () => {
+
+    test ('it should take inputed age and should return age', () => {
+      expect (inputedAge).toEqual(24);
+    });
   })
-})
